b07211672e3e5bfd96fecc78f71c37e94386f03e
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-03 06:58:23 +0200

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -1,1 +1,41 @@
 #pragma once
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class MainGame {
+  public:
+    ~MainGame();
+    void run();
+  
+  
+ private:
+      void init();
+      void initRenderers();
+      void initBalls();
+      void drawHud();
+      void update(float deltaTime); 
+      void draw();
+      void processInput();
+  
+  int m_screenwidth = 0;
+  int m_screen
