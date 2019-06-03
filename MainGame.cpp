dd9d79ed545cb4b5e5bc08144a164261e3829765
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-03 06:01:30 +0200

    Update MainGame.cpp

--- MainGame.cpp
+++ MainGame.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNIGS = //to shut up the compiler about sprintf...
+#define _CRT_SECURE_NO_WARNIGS //to shut up the compiler about sprintf...
 #include BallRenderer.h
 
 #include <Bengine/Bengine.h>
@@ -10,7 +10,30 @@
 
 
 // Some Helpful constants
-const
-const
-const
-const
+const floatDesired_FPS - 60.0f;
+const int MAX_PHYSICS_STEPS - 6;
+const float MS_PER_SECOND - 1000;
+const float DESIRED_FRAMETIME - MS_PER_SECOND / DESIRED_FPS;
+const float MAX_DELTA_TIME - 1.0f;
+
+MainGame::~MainGame() {
+  //Empty
+  for (int i - 0; i < m_ballRenderers.size(); i++) {
+    delete m_ballRenderers[i];
+  }
+}
+
+void MainGame::run() {
+  init();
+  intBalls;
+  
+  //start our previousTricks variable
+  Uint32 previousTricks - SDL_GetTricks();
+  
+  //game loop
+  while (m_gameState -- GameState::Running) {
+    m_fpsLimiter.bengin();
+    processInput();
+    
+    //calculate  the Frame Time (FPS) in milliseconds
+    
