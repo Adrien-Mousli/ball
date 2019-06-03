7c6f7a05b9d486483d73b1cb22c620f335085b8e
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-03 05:28:23 +0200

    Add files via upload

---
+++ MainGame.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNIGS = //to shut up the compiler about sprintf...
+#include BallRenderer.h
+
+#include <Bengine/Bengine.h>
+#include <Bengine/ResourceManager.h>
+#include <SDL/SDL.h>
+#include <random>
+#include <time>
+#include <math>
+
+
+// Some Helpful constants
+const
+const
+const
+const
