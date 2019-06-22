542fb20d76c2640d6162e3abc7a876a6b4576a6e
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-22 19:31:42 +0200

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -38,4 +38,24 @@ class MainGame {
       void processInput();
   
   int m_screenwidth = 0;
-  int m_screen
+  int m_screenHeight = 0;
+  
+  std::vector<Ball> m_balls; // toutes boules
+	std::unique_ptr<Grid> m_grid;
+
+	std::vector<BallRenderer*> > m_BallRenderers;
+
+	BallController m_BallControler; //< Controlle des balles
+
+	Bengine::Window m_window; ///< la fenêtre principale
+	Bengine::SpriteBatch m_spriteBatch; ///< Rend toutes les balles
+	std::unique_ptr<Bengine::SpriteFont> m_spriteFont;
+	Bengine::Camera2D m_camera; ///< rend la scène
+	Bengine::InputManager;
+	Bengine::GlSLProgram m_textureProgram; ///< shader pour la texture
+
+	Bengine::fpsLimiter m_fpsLimiter; ///< calcul et limite les fps (frame par secondes)
+	float m_fps = 0.0f;
+
+	GameState m_gameState = GameState::RUNNING;
+};
