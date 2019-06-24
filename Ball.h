974698947bca32022543bd636b4649b036d7b8f1
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-24 06:56:46 +0200

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -14,4 +14,82 @@ class Ball {
 	
 	
 	}
-m_program->AddAtribute
+
+	public var speed : Point;
+	public var ballRadius : int;
+
+	public function Ball(xPos:int, yPos : int, rad : int)
+{
+		speed = new Point((Math.random() * 3) + 2, (Math.random() * 3) + 2);
+		x = xPos;
+		y = yPos;
+		ballRadius = rad;
+}
+
+	public function update() :void {
+		x += speed.x;
+		y += speed.y;
+		if (x >= 300 - ballRadius && speed.x > 0) speed.x = -speed.x;
+		if (x <= ballRadius && speed.x < 0) speed.x = -speed.x;
+		if (y >= 200 - ballRadius && speed.y > 0) speed.y = -speed.y;
+		if (y <= ballRadius && speed.y < 0) speed.y = -speed.y;
+
+			graphics.clear();
+			graphics.beginFill(0xFF0000);
+			graphics.drawCircle(0, 0, ballRadius);
+			graphics.endFill();
+			trace(x);
+}
+
+}
+{
+	removeEventListener(Event.ADDED_TO_STAGE, init);
+	// entry point
+	stage.addEventListener(Event.ENTER_FRAME, update);
+	addChild(game);
+}
+ {
+	{
+private function update(e:Event) :void { game.update(); }
+
+	}
+
+}
+
+ distance = Math.sqrt(
+	 ((firstBall.x - secondBall.x) * (firstBall.x – secondBall.x))
+	 + ((firstBall.y - secondBall.y) * (firstBall.y – secondBall.y))
+ );
+ 
+ if (distance < firstBall.radius + secondBall.radius)
+ {
+	 //balls have collided
+ };
+
+ 
+ {
+	 NewVelX =
+		 (firstBall.speed.x * (firstBall.mass – secondBall.mass) + (2 * secondBall.mass * secondBall.speed.x))
+		 / (firstBall.mass + secondBall.mass);
+
+	 {
+		 NewVelX = (5 * (10 - 10) + (2 * 10 * -1)) / (10 + 10)
+			 = (5 * 0) + (-20) / 20
+			 = -20 / 20
+			 = -1
+	 }
+		{
+			 newVelX1 = (firstBall.speed.x * (firstBall.mass – secondBall.mass) + (2 * secondBall.mass * secondBall.speed.x)) / (firstBall.mass + secondBall.mass);
+			 newVelY1 = (firstBall.speed.y * (firstBall.mass – secondBall.mass) + (2 * secondBall.mass * secondBall.speed.y)) / (firstBall.mass + secondBall.mass);
+			 newVelX2 = (secondBall.speed.x * (secondBall.mass – firstBall.mass) + (2 * firstBall.mass * firstBall.speed.x)) / (firstBall.mass + secondBall.mass);
+			 newVelY2 = (secondBall.speed.y * (secondBall.mass – firstBall.mass) + (2 * firstBall.mass * firstBall.speed.y)) / (firstBall.mass + secondBall.mass);
+		}
+
+			{
+				 firstBall.x = firstBall.x + newVelX1;
+				 firstBall.y = firstBall.y + newVelY1;
+				 secondBall.x = secondBall.x + newVelX2;
+				 secondBall.y = secondBall.y + newVelY2;
+			}
+
+ };
