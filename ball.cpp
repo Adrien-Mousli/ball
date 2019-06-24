d0a4be37ee7ba318f133e4efc3015b9ddf47f9ee
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-06-24 06:55:39 +0200

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -7,3 +7,22 @@ for (auto& ball : balls) {
 	const glm::vec4 destRect(ball.position.x - ball.radius, ball.position.y);
 	spriteBatch.draw(destRect, uvRect, ball.textureId, 0.0f, ball.color);
 
+(const int "firstBall", const int "secondBall") {
+		
+		if (firstBall.x + firstBall.radius + secondBall.radius > secondBall.x
+			&& firstBall.x < secondBall.x + firstBall.radius + secondBall.radius
+			&& firstBall.y + firstBall.radius + secondBall.radius > secondBall.y
+			&& firstBall.y < seconBall.y + firstBall.radius + secondBall.radius)
+	}
+{
+	distance = Math.sqrt(
+	((firstBall.x - secondBall.x) * (firstBall.x – secondBall.x))
+	+ ((firstBall.y - secondBall.y) * (firstBall.y – secondBall.y))
+	);
+		if (distance < firstBall.radius + secondBall.radius)
+		
+			a = firstBall.x – secondBall.x;
+			b = firstBall.y – secondBall.y;
+		{
+		//balls have collided
+		}
